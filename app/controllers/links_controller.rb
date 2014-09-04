155502e87746b86159782252df90734ee1490293
Meghan Lindsley & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-04 11:09:37 -0700

    create form for adding new links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -8,12 +8,12 @@ class LinksController < ApplicationController
   end
 
   def create
-    @link = Link.create(params[:link])
+    @link = Link.create(links_params)
     if @link.valid?
-      flash[:notice] "You've successfully added a new link"
+      flash[:notice] = "You've successfully added a new link"
       redirect_to link_path(@link)
     else
-      flash[:alert] "Invalid entry, please try again."
+      flash[:alert] = "Invalid entry, please try again."
       render "new"
     end
   end
@@ -24,13 +24,17 @@ class LinksController < ApplicationController
 
   def update
     @link = Link.find(params[:id])
-    if @link.update(params[:link])
-      flash[:notice] "You've successfully edited the link"
+    if @link.update(links_params)
+      flash[:notice] = "You've successfully edited the link"
       redirect_to edit_link_path(@link)
     else
-      flash[:alert] "Invalid entry, please try again"
+      flash[:alert] = "Invalid entry, please try again"
       render "edit"
     end
   end
 
+private
+  def links_params
+    params.require(:link).permit(:id, :headline, :website)
+  end
 end
