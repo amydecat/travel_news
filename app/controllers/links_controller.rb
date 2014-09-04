4046e9b3348b6e52379e3d99ac13d64c88ba9c41
Meghan Lindsley & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-04 10:48:08 -0700

    add creation and update methods to links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,35 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.create(params[:link])
+    if @link.valid?
+      flash[:notice] "You've successfully added a new link"
+      redirect_to link_path(@link)
+    else
+      flash[:alert] "Invalid entry, please try again."
+      render "new"
+    end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(params[:link])
+      flash[:notice] "You've successfully edited the link"
+      redirect_to edit_link_path(@link)
+    else
+      flash[:alert] "Invalid entry, please try again"
+      render "edit"
+    end
+  end
+
 end
