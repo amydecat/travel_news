7b59b9e775ca99fa9b17d25a4c5dd5c07130b0b5
Meghan Lindsley & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-04 10:26:25 -0700

    fix database.yml file

--- db/migrate/20140904172110_create_links.rb
+++ db/migrate/20140904172110_create_links.rb
@@ -1,6 +1,10 @@
 class CreateLinks < ActiveRecord::Migration
   def change
     create_table :links do |t|
+      t.varchar :headline
+      t.varchar :website
+
+      t.timestamps
     end
   end
 end
