f5543a1237aa2430f3e64d347017c2eaf6847f61
Meghan Lindsley & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-05 11:51:25 -0700

    add association comment has many comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ActiveRecord::Base
   validates :opinion, :presence => true
   belongs_to :link
   belongs_to :commentable, :polymorphic => true
-  # has_many :comments, :as => :commentable
+  has_many :comments, :as => :commentable
 end
--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -5,5 +5,5 @@ describe Comment do
   it {should validate_presence_of :link_id}
   it {should belong_to :link}
   it {should belong_to :commentable}
-  # it {should have_many :comments, :as => :commentable }
+  it {should have_many :comments}
 end
