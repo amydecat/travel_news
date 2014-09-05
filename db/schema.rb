a70aead2aa4f43f11df0b21500ee032ad4428073
Meghan Lindsley & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-05 11:50:46 -0700

    add association comment belongs to commentable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905163656) do
+ActiveRecord::Schema.define(version: 20140905183823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140905163656) do
     t.text     "opinion"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
   end
 
   create_table "links", force: true do |t|
--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 describe Comment do
   it {should validate_presence_of :opinion}
   it {should validate_presence_of :link_id}
-
   it {should belong_to :link}
+  it {should belong_to :commentable}
+  # it {should have_many :comments, :as => :commentable }
 end
