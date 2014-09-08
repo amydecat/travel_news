5e4a3b51669081f431b8b2f2f3bddc0a5b90ee17
Meghan Lindsley & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-08 15:41:05 -0700

    add has_many associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :password_digest, presence: true
   validates_uniqueness_of :email
+  has_many :links
+  has_many :comments
+  has_many :votes
+
+
   has_secure_password
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,4 +4,7 @@ describe User do
   it {should validate_presence_of :email}
   it {should validate_presence_of :password_digest}
   it {should validate_uniqueness_of :email}
+  it {should have_many :links}
+  it {should have_many :votes}
+  it {should have_many :comments}
 end
